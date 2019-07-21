9123b989d7d744616229e830f14d6fef967db0bf
Manuel Gelsen <manuel.gelsen@gmail.com>
2019-07-21 08:07:05 +0200

    neues favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ module.exports = {
             { name: 'language', content: 'de' },
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/favicon-16x16.png' },
+            { rel: 'icon', type: 'image/png', sizes: "32x32", href: '/favicon-32x32.png' }
         ]
     },
     /*
