99de8262f308d6102f83cd18cd434c72aa3257f2
Manuel Gelsen <manuel.gelsen@gmail.com>
2020-05-16 10:37:16 +0200

    refactoring

--- static/databinding/databinding.js
+++ static/databinding/databinding.js
@@ -1,190 +1,7 @@
 import $ from './ownquery.js';
-//import StateMachine from './Statemachine.js';
-
-function noop() {};
-const globalCache = {
-    onInit: [],
-    onElementInit: [],
-    onRender: []
-}
-
-function StateMachine(_state = {}) {
-    const watched = {};
-
-    const cache = {
-        computed: {},
-        bindings: {},
-        dependencies: {}
-    };
-
-    function randomId(prefix) {
-        return prefix + Math.random().toString(36).substring(2, 15);
-    }
-
-    const state = new Proxy(_state, {
-        set(target, property, value) {
-            target[property] = value;
-            render(target, property, value);
-
-            for (let dependency of cache.dependencies[property]) {
-                render(target, dependency, state[dependency]);
-            }
-
-            return true;
-        },
-        get(obj, prop) {
-            const value = obj[prop];
-            return value;
-        }
-    });
-
-    function compute(text) {
-        const _values = values();
-        const paramValues = Object.keys(_values).map(key => _values[key]);
-
-        const fn = toFn(text);
-        const result = fn(...paramValues); //wirft einen Fehler, wenn invalide
-        return result;
-    }
-
-    function toFn(text) {
-
-        const _values = values('parsable');
+import StateMachine from './Statemachine.js';
 
 
-        const paramKeys = Object.keys(_values);
-        try {
-            const codeNonString = 'return ' + text + ';';
-            const fn = Function(...paramKeys, codeNonString);
-            return fn;
-        } catch (e) {
-            try {
-                const codeString = 'return `' + text + '`;';
-                const fn = Function(...paramKeys, codeString);
-                return fn;
-            } catch (e) {
-                console.log('code can not be parsed', codeNonString)
-            }
-
-        }
-    }
-
-    function addDependencies(fn, name, elemid, elemkey, keyprefix = "") {
-        const fnText = fn.toString();
-        Object.keys(state).forEach(key => {
-            if (!fnText.match(new RegExp("\\b" + keyprefix + key + "\\b")))
-                return;
-
-            if (!cache.dependencies[key]) {
-                cache.dependencies[key] = [];
-            }
-
-            if (key !== name) {
-                const matchid = elemid ? elemid + '_' + elemkey : name;
-                cache.dependencies[key].push(matchid);
-                cache.bindings[matchid] = {
-                    elemid,
-                    name,
-                    fn: toFn(fn)
-                }
-            }
-        });
-    }
-
-
-    function values(parsable) {
-        const retObj = {};
-
-        Object.keys(state).forEach(key => {
-            let value = state[key];
-            let valueText = value;
-
-            if (typeof value == 'function') {
-                value = value(state);
-                valueText = value;
-            }
-
-            if (value == null)
-                valueText = "''";
-
-            if (typeof value == 'string' && parsable)
-                valueText = `'${value}'`;
-
-            if (typeof value == 'object' && parsable)
-                valueText = JSON.stringify(value);
-
-            retObj[key] = valueText;
-        });
-
-        return retObj;
-    }
-
-    function watch(key, fn) {
-        if (!watched[key])
-            watched[key] = fn;
-    }
-
-    for (let initFn of globalCache.onInit) {
-        const result = initFn({ state, compute }) || {};
-
-        if (result.onElementInit)
-            globalCache.onElementInit.push(result.onElementInit);
-        if (result.onRender)
-            globalCache.onRender.push(result.onRender);
-    }
-
-    $('*').elems.forEach(elem => {
-        for (let onElementInitFn of globalCache.onElementInit) {
-            onElementInitFn(elem);
-        }
-
-        const elemId = elem.dataset.id || randomId('id_')
-
-        Object.keys(elem.dataset).forEach(key => {
-            const id = randomId('rnd');
-            const fnToCompute = elem.dataset[key];
-
-            cache.computed[id] = toFn(fnToCompute);
-            addDependencies(fnToCompute, id, elemId, key);
-
-            elem.dataset.id = elemId;
-        })
-
-
-    });
-
-    const render = (state, prop, value) => {
-        console.log('render', prop, value);
-
-        for (let onRenderFn of globalCache.onRender) {
-            onRenderFn(state, prop, value);
-        }
-
-    };
-
-    Object.keys(state).forEach(key => {
-        if (typeof state[key] == 'function') {
-            cache.computed[key] = state[key];
-            addDependencies(state[key], key);
-        }
-
-        render(state, key, state[key])
-    });
-
-    return {
-        state,
-        values: values(),
-        watch,
-        cache
-    }
-};
-
-
-function register(fn) {
-    globalCache.onInit.push(fn || noop);
-}
-
-StateMachine.register = register;
 
 
 /*
@@ -220,7 +37,7 @@ StateMachine.register(function onRenderDataBind({ compute }) {
 
 StateMachine.register(function onRenderDataClass({ compute }) {
     return {
-        onRender(state) {
+        onRender(state, prop, value) {
             $(`[data-class]`).elems.forEach(elem => {
                 const classes = elem.dataset.class;
                 const result = compute(classes, state);
@@ -243,7 +60,7 @@ StateMachine.register(function onRenderDataClass({ compute }) {
 
 StateMachine.register(function onRenderDataStyle({ compute }) {
     return {
-        onRender(state) {
+        onRender(state, prop, value) {
             $(`[data-style]`).elems.forEach(elem => {
                 const styleInline = elem.dataset.style;
                 const result = compute(styleInline, state);
@@ -268,7 +85,7 @@ StateMachine.register(function onRenderStyleXyz({ compute }) {
                 elem.setAttribute('_datastyle', "");
             }
         },
-        onRender(state) {
+        onRender(state, prop, value) {
             $(`[_datastyle]`).elems.forEach(elem => {
                 const datastyle = Object.keys(elem.dataset).filter(key => key.startsWith('style.'));
 
@@ -292,7 +109,7 @@ StateMachine.register(function onRenderClassXyz({ compute }) {
                 elem.setAttribute('_datacls', "");
             }
         },
-        onRender(state) {
+        onRender(state, prop, value) {
             $(`[_datacls]`).elems.forEach(elem => {
                 const dataclasses = Object.keys(elem.dataset).filter(key => key.startsWith('class.'));
 
@@ -328,7 +145,7 @@ StateMachine.register(function addDataModelEvents({ state }) {
 
 StateMachine.register(function onRenderDataShow({ compute }) {
     return {
-        onRender(state) {
+        onRender(state, prop, value) {
             $(`[data-show]`).elems.forEach(elem => {
                 const showCondition = elem.dataset.show;
                 const result = compute(showCondition, state);
